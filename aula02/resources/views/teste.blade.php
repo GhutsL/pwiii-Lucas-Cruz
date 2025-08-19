527e3a9bfed95231413cda2dd188dca26b666211
Lucas Cruz <lucasmatiasc2004@gmail.com>
2025-08-18 22:20:21 -0300

    Aprendendo sobre tailwind

---
+++ aula02/resources/views/teste.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+  <head>
+    @vite('resources/css/app.css')
+  </head>
+  <body>
+<div class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+  <div>
+    <div class="text-xl font-medium text-black dark:text-white">ChitChat</div>
+    <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
+    <button class="bg-sky-500 disabled:hover:bg-sky-500 rounded">Save changes</button>
+    <div class="flex flex-center bg-blue-500 rounded">
+     <div class="basis-64   ">01</div>
+     <div class="basis-64   ">02</div>
+     <div class="basis-128   ">03</div>
+    </div>
+  </div>
+</div >
+<div>
+
+</div>    
+  </body>
+</html>
